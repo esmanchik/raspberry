d92e743b795985874c54b091462f736d5ce175df
Sasha <esmanchik@gmail.com>
2017-04-11 16:37:09 +0000

    Added router setup

--- router.sh
+++ router.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+sudo cp dnsmasq.conf /etc/dnsmasq.conf
+sudo cp iptables.ipv4.nat /etc/iptables.ipv4.nat
+
 sudo sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward"
 sudo iptables-restore < /etc/iptables.ipv4.nat
 sudo wvdial
