8c81038562a47226cc6ad288daf1b0516a3a5713
Sasha <esmanchik@gmail.com>
2017-04-15 18:17:47 +0000

    Moved access point setup part to ap.sh

--- ap.sh
+++ ap.sh
@@ -2,4 +2,6 @@
 sudo apt-get install usb-modeswitch wvdial hostapd dnsmasq
 sudo cp 40-modemswitch.rules /etc/udev/rules.d/40-modemswitch.rules
 sudo cp wvdial.conf /etc/wvdial.conf
+sudo cp dnsmasq.conf /etc/dnsmasq.conf
+sudo cp iptables.ipv4.nat /etc/iptables.ipv4.nat
 
--- router.sh
+++ router.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-sudo cp dnsmasq.conf /etc/dnsmasq.conf
-sudo cp iptables.ipv4.nat /etc/iptables.ipv4.nat
-
 sudo sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward"
 sudo iptables-restore < /etc/iptables.ipv4.nat
 sudo wvdial
